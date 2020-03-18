a38cdd2aa3744c61cefbee08b41b6802c2e6e3e4
alikurtulush92 <kopanarya92@gmail.com>
2020-03-18 23:51:56 +0000

    Added quiz application with vue.js

---
+++ vuequizzapp/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <Header :numCorrect="numCount" :numTotal="numTotal" />
+        <b-container class="bv-example-row">
+          <b-row>
+              <b-col sm="6" offset="3">
+                 <QuestionBox 
+                 v-if="questions.length"
+                 :currentQuestion="questions[index]"
+                 :next="next"
+                 :increment = "increment"
+                 />
+              </b-col>
+         </b-row>
+        </b-container>
+    
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import QuestionBox from './components/QuestionBox'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    QuestionBox
+  },
+  data(){
+    return {
+      questions : [],
+      index: 0,
+      numCount: 0,
+      numTotal: 0
+    }
+  },
+  methods: {
+     next() {
+       this.index++
+     },
+     increment(isCorrect){
+       if(isCorrect){
+          this.numCount++
+       }
+       this.numTotal++
+
+     }
+  },
+  mounted: function(){
+    fetch('https://opentdb.com/api.php?amount=10&category=9&type=multiple',{
+      method:'get'
+    })
+     .then((response) => {return response.json()} )
+     .then((jsonData) => {
+       this.questions = jsonData.results
+     })
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vuequizzapp/src/components/QuestionBox.vue
@@ -0,0 +1,114 @@
+<template>
+    <div class="quesiton-box-container"> 
+        <b-jumbotron >
+            <template v-slot:lead>
+             {{currentQuestion.question}}
+            </template>
+            <hr class="my-4">
+            <b-list-group >
+                 <b-list-group-item
+                  v-for="(answer, index) in answers"
+                  :key="index"
+                  @click="selectAnswer(index)"
+                  :class="answerClass(index)">
+                    {{answer}}
+                   </b-list-group-item>
+            </b-list-group>
+            <b-button variant="primary" @click="submitAnswer" :disabled="selectedIndex === null || answered">Submit</b-button>
+            <b-button @click="next" variant="success" >Next</b-button>
+        </b-jumbotron>
+    </div>
+</template>
+<script>
+import _ from 'lodash'
+export default {
+    props:{
+        currentQuestion: Object,
+        next: Function,
+        increment: Function
+    },
+    data(){
+       return{
+           selectedIndex: null,
+           correctIndex: null,
+           shuffledAnswers: [],
+           answered: false
+       }
+    },
+    computed:{
+      answers() {
+           let answers = [...this.currentQuestion.incorrect_answers]
+           answers.push(this.currentQuestion.correct_answer)
+           return answers
+      }
+    
+    },
+    methods:{
+        selectAnswer(index) {
+            this.selectedIndex = index
+        },
+        shuffleAnswers(){
+            let answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer]
+            this.shuffledAnswers = _.shuffle(answers)
+            this.correctIndex = this.shuffledAnswers.indexOf(this.currentQuestion.correct_answer)
+        },
+        submitAnswer(){
+            let isCorrect = false
+            if(this.selectedIndex === this.correctIndex){
+                isCorrect = true
+            }
+            this.increment(isCorrect)
+            this.answered = true
+        },
+        answerClass(index){
+            let answerClass =''
+            if( !this.answered && this.selectedIndex === index){
+                answerClass = 'selected'
+            }
+            else if( this.answered && this.correctIndex === index){
+                answerClass = 'correct'
+            }
+            else if(this.answered && this.selectedIndex === index && this.correctIndex !== index){
+                 answerClass = 'incorrect'
+            }
+                return answerClass
+        }
+    },
+    watch: {
+      currentQuestion:{
+          immediate:true,
+          handler() {
+              this.selectedIndex = null
+              this.shuffleAnswers()
+              this.answered= false
+          }
+        
+      }
+    }
+}
+</script>
+<style scoped>
+.quesiton-box-container{
+    text-align: center;
+}
+.list-group{
+    margin-bottom: 15px;
+}
+.list-group-item:hover{
+    background: #eee;
+    cursor: pointer;
+}
+
+.btn {
+    margin:0 5px;
+}
+.selected{
+    background: lightblue;
+}
+.correct{
+    background: green;
+}
+.incorrect{
+    background: red;
+}
+</style>
